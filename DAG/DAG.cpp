6728924a77443cab43a43f361cdaeca81d6313d5
whoseJam <276707964@qq.com>
2023-07-29 19:01:06 +0800

    update

--- DAG/DAG.cpp
+++ DAG/DAG.cpp
@@ -1,11 +1,12 @@
-#include<thpool.hpp>
-#include<iostream>
-#include<ctime>
+#include "thpool.h"
+#include <iostream>
+#include <ctime>
+#include <mutex>
 
 using namespace std;
 
-thpool pool;
-pthread_mutex_t mut;
+thpool pool(4);
+mutex mut;
 
 int R(int a,int b){
 	return (rand()%(b-a+1)+a);
@@ -13,26 +14,24 @@ int R(int a,int b){
 
 vector<int> G[100];
 
-struct data{
+struct Data{
 	int ID;
 	int inDegree;
 	string str;
 }D[100];
 
-void* Task(void* d){
-	data* myData=reinterpret_cast<data*>(d);
+void Task(Data* myData){
 	cout<<(*myData).ID<<"\n";
 
-	pthread_mutex_lock(&mut);
+	lock_guard<mutex> lock(mut);
 	for(int i=0;i<G[(*myData).ID].size();i++){
 		int v=G[(*myData).ID][i];
 		D[v].inDegree--;
 		if(D[v].inDegree==0){
-			Job tmp;tmp.init(Task,&D[v],1);
-			pool.addJob(tmp);
+			Job tmp(Task, &D[v]);
+			pool.add(tmp);
 		}
 	}
-	pthread_mutex_unlock(&mut);
 }
 
 void Link(int u,int v){
@@ -43,27 +42,23 @@ int n;
 
 int main(int argc,char** args){
 	srand(time(NULL));
-	pthread_mutex_init(&mut,nullptr);
 
-	pool.init(16);n=atoi(args[1]);
-	for(int i=1;i<=n;i++){
+	n = atoi(args[1]);
+	for(int i=1; i<=n; i++) {
 		D[i].ID=i;
 		D[i].str="helloworld";
 	}
-	for(int i=2;i<=n;i++){
+	for(int i=2; i<=n; i++) {
 		while(R(1,4)<=3){
 			Link(R(1,i-1),i);
 		}
 	}
 	for(int i=1;i<=n;i++)
 		if(D[i].inDegree==0){
-			Job tmp;
-			tmp.init(Task,&D[i],1);
-			pool.addJob(tmp);
+			Job tmp(Task, &D[i]);
+			pool.add(tmp);
 		}
-	pool.waitJob();
+	pool.wait();
 	cout<<"all have been done\n";
-	
-	pthread_mutex_destroy(&mut);
 	return 0;
 }
